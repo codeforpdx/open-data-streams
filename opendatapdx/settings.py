b0f165fa5588f093a0c4bfbc34b2bf011423075d
Austen Holmberg <aholm2@pdx.edu>
2018-08-25 00:48:29 -0700

    fixing statics

--- opendatapdx/settings.py
+++ opendatapdx/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
