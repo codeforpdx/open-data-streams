d9c16264f442fb9001c5c1fd79fd77b1e7012f55
Ben <maiahee@gmail.com>
2018-09-28 18:56:05 -0700

    Beginnings of backend for dashboard
    and rudimentery frontend for testsing

--- cataloger/views.py
+++ cataloger/views.py
@@ -5,3 +5,11 @@ from .models import Dataset, Distribution, Schema
 
 def index(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    datasets = None
+    if request.session.has_key('user_id'):
+      datasets = list(Dataset.objects.filter(publisher_id__exact = request.session['user_id']))
+    else:
+        datasets = [{'test element 1' : 'test val 1'}, {'test element 2' : 'test val 2'}]
+    return render(request, 'dashboard.html', {'datasets' : datasets})
