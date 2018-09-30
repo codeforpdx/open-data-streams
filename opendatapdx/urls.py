9cac4e3d76d3dd84100e76fee3e14e68dd18b818
EddieKelley <kelleye@pdx.edu>
2018-09-29 17:21:11 -0700

    re-added some items left out after rebasing (#2)
    * Added user authentication templates
    
    Added user auth templates (based on this: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Authentication)
    Moved nav bar HTML to base.html template
    
    * Registration backend creation
    
    Added registration page
    Modified Profile model to use AbstractUser
    
    * User registration working
    
    Got user registration backend and simple frontend working
    
    * Added user login/logout links
    
    Added conditional user login and logout list item to nav bar
    
    * Modified register button
    
    Modified register button for display only when no users are logged in

--- opendatapdx/urls.py
+++ opendatapdx/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
