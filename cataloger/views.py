11bd8a55539319e9b1b3cd36e4379b049d274e2a
Eddie Kelley <kelleye@pdx.edu>
2018-10-02 23:38:28 -0700

    Superuser navbar links
    Added links for superusers in navbar;
    Changed query filter for Office lookup;

--- cataloger/views.py
+++ cataloger/views.py
@@ -46,7 +46,7 @@ def register(request):
         # this is a POST request
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            profile = Profile.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'], Office.objects.all().filter(description = request.POST['office']).first())
+            profile = Profile.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'], Office.objects.filter(description = request.POST['office']).first())
             profile.save()
             
             user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
