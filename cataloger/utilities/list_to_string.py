15c5ae63a0e0f9ff958481de7f801eace7c92c26
DanielRBramblett <bram4@pdx.edu>
2018-11-11 22:36:25 -0800

    Fixed quotation problem

--- cataloger/utilities/list_to_string.py
+++ cataloger/utilities/list_to_string.py
@@ -3,7 +3,7 @@ def list_to_string(input_list):
     if len(input_list) > 1:
         for current_index in input_list:
             output_string += str(current_index) + ','
-        output_string = ' " ' + output_string[:-1] + ' " '
+        output_string = output_string[:-1]
     elif len(input_list) == 1:
         output_string = str(input_list[0])
     return output_string
