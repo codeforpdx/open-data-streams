d5abf867a74d1a591485a517a2fc941e98defeb5
Ben <maiahee@gmail.com>
2018-11-12 22:47:38 -0800

    fix dataset_filters
    added one for schema, no idea if it works

--- api/dataset_filter.py
+++ api/dataset_filter.py
@@ -1,4 +1,4 @@
-from cataloger.models import Dataset, Profile
+from cataloger.models import Dataset, Profile, Schema
 
 COMPLETED_DATASETS = Dataset.objects.filter(complete=True)
 PUBLISHED_DATASETS = Dataset.objects.filter(published=True)
@@ -8,8 +8,13 @@ def filtered_datasets(request):
     if request.user.is_superuser:
         datasets = COMPLETED_DATASETS
     elif request.user.is_authenticated:
-        datasets = COMPLETED_DATASETS.filter(publisher=Profile(id=request.user.id))
-        datasets.union(PUBLISHED_DATASETS)
+        own_datasets = COMPLETED_DATASETS.filter(publisher=Profile(id=request.user.id))
+        datasets = own_datasets.union(PUBLISHED_DATASETS)
     else:
         datasets = PUBLISHED_DATASETS
     return datasets
+
+
+# UNTESTED
+# def filtered_schema(request):
+#     return filtered_datasets(request).select_related('schema')
