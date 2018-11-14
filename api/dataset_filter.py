ff4caae1e342bd7d10bdfe280c865e80d266c2c5
Ben <maiahee@gmail.com>
2018-11-13 19:18:41 -0800

    fixed filters better
    because who knows what union does...

--- api/dataset_filter.py
+++ api/dataset_filter.py
@@ -1,4 +1,5 @@
 from cataloger.models import Dataset, Profile, Schema
+from django.db.models import Q
 
 COMPLETED_DATASETS = Dataset.objects.filter(complete=True)
 PUBLISHED_DATASETS = Dataset.objects.filter(published=True)
@@ -8,8 +9,7 @@ def filtered_datasets(request):
     if request.user.is_superuser:
         datasets = COMPLETED_DATASETS
     elif request.user.is_authenticated:
-        own_datasets = COMPLETED_DATASETS.filter(publisher=Profile(id=request.user.id))
-        datasets = own_datasets.union(PUBLISHED_DATASETS)
+        datasets = COMPLETED_DATASETS.filter(Q(publisher=Profile(id=request.user.id)) | Q(published=True))
     else:
         datasets = PUBLISHED_DATASETS
     return datasets
